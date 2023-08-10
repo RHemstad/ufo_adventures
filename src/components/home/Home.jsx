ca5c18093969f8fb3bcdc73c21a2e42d4f73934d
RHemstad <28883539+RHemstad@users.noreply.github.com>
2023-08-09 21:09:32 -0400

    work in progress on homepage

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,11 @@
 
 import "./home.css";
 import React from 'react';
-import { Link} from "react-router-dom";
+import {Link} from "react-router-dom";
+import Button from '../button/Button';
 
 import placeholder from '../../assets/images/600x400.svg';
+import ufo from '../../assets/images/ufo.svg';
 
 const Home = () => {
   return (
@@ -15,13 +17,27 @@ const Home = () => {
 {/* ****************************************** */} 
 
 <section id="hero" className="hero">
+
+<div id="animation">
+          <div id="ufo" className="x">
+            <img className="y" src={ufo} alt="spaceship"/>
+          </div>
+        </div>
+
+
+
+
+
+
     <article>
         <h5>ONE WEEKEND ONLY</h5>
         <h2>Your Unforgettable Alien Abduction Awaits!</h2>
 
         <p>Experience the ultimate adventure with UFO Adventures – an authentic Alien abduction awaits you! Choose from our Standard or VIP Abduction Packages and step into a world beyond imagination.</p>
 
-        <button>Join Today</button>
+        <Link to="/tickets">
+        <Button className="primary-button">Join Today</Button>
+        </Link>
     </article>
 
 
@@ -46,14 +62,15 @@ const Home = () => {
 <h3>Standard Abduction Experience</h3>
 <p>The Standard Abduction Experience promises an extraordinary cosmic adventure that will leave you in awe. Join our extraterrestrial guides as they whisk you away on a journey of discovery through the galaxies. Witness stunning interstellar landscapes. Immerse yourself in the wonders of their advanced technology. Enjoy a mesmerizing blend of adventure, curiosity, and exploration.</p>
 
-<button>Purchase Tickets</button>
+
+<Button className="primary-button" to="/tickets">Purchase Tickets</Button>
 </article>
 
 <article>
 <div><img src={placeholder} alt="placeholder"/></div>
 <h3>VIP Abduction Package</h3>
 <p>The VIP Abduction Package offers an unparalleled interstellar experience beyond your wildest dreams. As a privileged voyager, you'll be treated to exclusive encounters with our advanced extraterrestrial hosts, gaining insights into their culture and technology. But that's not all - you'll also have the unique opportunity to participate in groundbreaking scientific experiments</p>
-<button>Purchase Tickets</button>
+<Button className="primary-button" to="/tickets">Purchase Tickets</Button>
 </article>
 
 </div>
@@ -67,7 +84,6 @@ const Home = () => {
 <section id="pya" className="pya">
 
 <div className="subheading">
-  <h5></h5>
   <h2>Planning Your Abduction</h2>
   <p>Ready to embark on an out-of-this-world journey? Start planning your abduction today and don't let any detail slip away - be fully equipped for your cosmic encounter! </p>
 </div>
@@ -76,19 +92,22 @@ const Home = () => {
 <article>
 <h3>Learn To Recognize UFOs</h3>
 <p>Can you spot the real UfOs from the fakes? Take on the UFO identity challenge and prove your mastery of the skies!  Sharpen your UFO-spotting abilities and become a certified extraterrestrial investigator!</p>
-<button>Select</button>
+
+
+<Button className="primary-button">Select</Button>
 </article>
 
 <article>
 <h3>Weather Checking Tool</h3>
 <p>Plan your abduction with confidence by checking the weather on the day and location of your scheduled encounter. Stay informed and be ready to explore the cosmos, rain or shine!</p>
-<button>Select</button>
+<Button className="primary-button">Select</Button>
+
 </article>
 
 <article>
 <h3>Packing Checklist</h3>
 <p>From space-age essentials to cosmic curiosities, we've got you covered. Get ready for the journey of a lifetime – check off your packing list now and gear up for an out-of-this-world experience!</p>
-<button>Select</button>
+<Button className="primary-button">Select</Button>
 </article>
 
 </div>
@@ -103,7 +122,6 @@ const Home = () => {
 <section id="testimonials" className="testimonials">
 
 <div className="subheading">
-  <h5></h5>
   <h2>Testimonials</h2>
 </div>
 
@@ -112,24 +130,24 @@ const Home = () => {
   <article className="card">
         <div className="card_picture">picture</div>
         <div className="card_body">
-          <p className="card_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum enim eu nibh finibus</p>
-         <cite title="Source Title">Mohamed Nady</cite>
+          <p className="card_text">"Nothing could have prepared me for this awe-inspiring experience. The Standard Abduction Package offered a genuine and authentic encounter with their friendly alien hosts."</p>
+         <cite title="Source Title">Richard Anderson, <br></br>Retired Aerospace Engineer</cite>
         </div>
   </article>
 
   <article className="card">
         <div className="card_picture">picture</div>
         <div className="card_body">
-          <p className="card_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum enim eu nibh finibus</p>
-         <cite title="Source Title">Mohamed Nady</cite>
+          <p className="card_text">"It was an unparalleled adventure that has ignited my creativity and expanded my understanding of the cosmos. UFO Adventures is a dream come true for any cosmic enthusiasts!"</p>
+         <cite title="Source Title">Alex Johnson, <br></br>Science Fiction Writer</cite>
         </div>
   </article>
 
   <article className="card">
         <div className="card_picture">picture</div>
         <div className="card_body">
-          <p className="card_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum enim eu nibh finibus</p>
-         <cite title="Source Title">Mohamed Nady</cite>
+          <p className="card_text">"I was a skeptic at first, but, it was a mind-blowing experience that I will cherish forever. UFO Adventures truly offers an unforgettable journey to the stars!"</p>
+         <cite title="Source Title">Sarah Thompson, <br></br>Astrophysics Enthusiast</cite>
         </div>
   </article>
 
