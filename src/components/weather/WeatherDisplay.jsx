9219537775dcd4d9b7b6268be23cd4199c396ddd
mhitt <133064678+mhitt@users.noreply.github.com>
2023-08-13 14:13:27 -0500

    added class names to div

--- src/components/weather/WeatherDisplay.jsx
+++ src/components/weather/WeatherDisplay.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 
+
+
+
 const WeatherDisplay = ({ weatherData }) => {
   return (
     <div>
       {weatherData && weatherData.main && weatherData.weather && (
-        <div>
-          <h2>
-            {weatherData.name}, {weatherData.sys && weatherData.sys.country}
-          </h2>
-          <p>Temperature: {Math.round(weatherData.main.temp)}°F</p>
-          <p>Weather: {weatherData.weather[0].description}</p>
-        </div>
+        <>
+          <div className='weatherDetails_div'>
+            <h2>
+              {weatherData.name}, {weatherData.sys && weatherData.sys.country}
+            </h2>
+            <p>Temperature: {Math.round(weatherData.main.temp)}°F</p>
+            <p>Weather: {weatherData.weather[0].description}</p>
+          </div>
+          <div className='weatherIcon_div'>
+
+          </div>
+        </>
       )}
     </div>
   )
